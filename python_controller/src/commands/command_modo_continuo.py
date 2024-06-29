a4af4b51488102edaa2bc8625591f671b3f2fab1
juan pablo saucedo <juan.s@ensolvers.com>
2024-06-29 19:54:04 -0300

    added modo continuo command

--- python_controller/src/commands/command_modo_continuo.py
+++ python_controller/src/commands/command_modo_continuo.py
@@ -1,10 +1,13 @@
 from .command import Command
+from ..constants.constants import CODE_MODO_CONTINUO
 
 class CommandModoContinuo(Command):
     def __init__(self, ser, log_path):
         super().__init__(ser, log_path)
 
     def execute(self):
-        print(self.__class__.__name__ + " executed")
+        print(self.__class__.__name__ + " executing")
+        self.send_code()
 
-        pass
+    def send_code(self):
+        self.ser.write(CODE_MODO_CONTINUO.to_bytes(1, 'big'))
