d4cdd8ca73a12104c078c8270b77bbfdcd4ecefb
juan pablo saucedo <juan.s@ensolvers.com>
2024-06-29 19:52:44 -0300

    added paso a paso command

--- python_controller/src/commands/command_modo_paso_a_paso.py
+++ python_controller/src/commands/command_modo_paso_a_paso.py
@@ -1,10 +1,15 @@
 from .command import Command
+from ..constants.constants import CODE_MODO_PASO_A_PASO
+
 
 class CommandModoPasoAPaso(Command):
     def __init__(self, ser, log_path):
         super().__init__(ser, log_path)
 
     def execute(self):
-        print(self.__class__.__name__ + " executed")
-
+        print(self.__class__.__name__ + " executing")
+        self.send_code()
         pass
+
+    def send_code(self):
+        self.ser.write(CODE_MODO_PASO_A_PASO.to_bytes(1, 'big'))
