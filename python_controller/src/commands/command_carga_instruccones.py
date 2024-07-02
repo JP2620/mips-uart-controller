96f22a3936c9f6270fc9819fece3c9d3332dadf8
juan pablo saucedo <juan.s@ensolvers.com>
2024-07-01 21:32:44 -0300

    fix carga instrucciones

--- python_controller/src/commands/command_carga_instruccones.py
+++ python_controller/src/commands/command_carga_instruccones.py
@@ -1,5 +1,7 @@
 from .command import Command
 from typing import List
+from ..constants.constants import CODE_CARGA_INSTR
+import time
 
 class CommandCargaInstrucciones(Command):
     def __init__(self, ser, log_path, instrucciones_path="python_controller/resources/instrucciones.mem"):
@@ -7,6 +9,8 @@ class CommandCargaInstrucciones(Command):
         super().__init__(ser, log_path)
 
     def execute(self):
+        self.send_code()
+        time.sleep(10)
         print(self.__class__.__name__ + " executed")
         instrucciones = self.read_instrucciones()
         self.send_instrucciones(instrucciones)
@@ -24,3 +28,6 @@ class CommandCargaInstrucciones(Command):
             print("sending bytes: ", bytes_to_send)
         print("instrucciones sent")
         pass
+
+    def send_code(self):
+        self.ser.write(CODE_CARGA_INSTR.to_bytes(1, 'big'))
